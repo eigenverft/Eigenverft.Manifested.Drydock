2b0afb9a4282e1611f16f05e92e1876b3abda2f3
Eigenverft <eigenverft@outlook.com>
2025-10-05 11:30:45 +0200

    fix

--- .github/workflows/cicd.ps1
+++ .github/workflows/cicd.ps1
@@ -118,62 +118,9 @@ $moduleManifest = "$moduleFolder/BlackBytesBox.Manifested.Git.psd1" -replace '[/
 Write-Host "===> Testing module manifest at: $moduleManifest" -ForegroundColor Cyan
 Test-ModuleManifest -Path $moduleManifest
 
+exit
+
 Publish-Module -Path $moduleFolder -Repository "PSGallery" -NuGetApiKey "$POWERSHELL_GALLERY"
 
 exit 0
-
-$currentBranch = Get-GitCurrentBranch
-$currentBranchRoot = Get-BranchRoot -BranchName "$currentBranch"
-$topLevelDirectory = Get-GitTopLevelDirectory
-
-#Branch too channel mappings
-$branchSegments = @(Split-Segments -InputString "$currentBranch" -ForbiddenSegments @("latest") -MaxSegments 2)
-$nugetSuffix = @(Translate-FirstSegment -Segments $branchSegments -TranslationTable @{ "feature" = "-development"; "develop" = "-quality"; "bugfix" = "-quality"; "release" = "-staging"; "main" = ""; "master" = ""; "hotfix" = "" } -DefaultTranslation "{nodeploy}")
-$nugetSuffix = $nugetSuffix[0]
-$channelSegments = @(Translate-FirstSegment -Segments $branchSegments -TranslationTable @{ "feature" = "development"; "develop" = "quality"; "bugfix" = "quality"; "release" = "staging"; "main" = "production"; "master" = "production"; "hotfix" = "production" } -DefaultTranslation "{nodeploy}")
-
-$branchFolder = Join-Segments -Segments $branchSegments
-$branchVersionFolder = Join-Segments -Segments $branchSegments -AppendSegments @( $calculatedVersion.VersionFull )
-$channelRoot = $channelSegments[0]
-$channelVersionFolder = Join-Segments -Segments $channelSegments -AppendSegments @( $calculatedVersion.VersionFull )
-$channelVersionFolderRoot = Join-Segments -Segments $channelSegments -AppendSegments @( "latest" )
-if ($channelSegments.Count -eq 2)
-{
-    $channelVersionFolderRoot = Join-Segments -Segments $channelRoot -AppendSegments @( "latest" )
-}
-
-
-Write-Output "BranchFolder to $branchFolder"
-Write-Output "BranchVersionFolder to $branchVersionFolder"
-Write-Output "ChannelRoot to $channelRoot"
-Write-Output "ChannelVersionFolder to $channelVersionFolder"
-Write-Output "ChannelVersionFolderRoot to $channelVersionFolderRoot"
-
-#Guard for variables
-Ensure-Variable -Variable { $calculatedVersion } -ExitIfNullOrEmpty
-Ensure-Variable -Variable { $currentBranch } -ExitIfNullOrEmpty
-Ensure-Variable -Variable { $currentBranchRoot } -ExitIfNullOrEmpty
-Ensure-Variable -Variable { $topLevelDirectory } -ExitIfNullOrEmpty
-Ensure-Variable -Variable { $nugetSuffix }
-Ensure-Variable -Variable { $NUGET_GITHUB_PUSH } -ExitIfNullOrEmpty -HideValue
-Ensure-Variable -Variable { $NUGET_PAT } -ExitIfNullOrEmpty -HideValue
-Ensure-Variable -Variable { $NUGET_TEST_PAT } -ExitIfNullOrEmpty -HideValue
-
-#Required directorys
-$artifactsOutputFolderName = "artifacts"
-$reportsOutputFolderName = "reports"
-
-$outputRootArtifactsDirectory = [System.IO.Path]::Combine($topLevelDirectory, $artifactsOutputFolderName)
-$outputRootReportResultsDirectory   = [System.IO.Path]::Combine($topLevelDirectory, $reportsOutputFolderName)
-$targetConfigAllowedLicenses = [System.IO.Path]::Combine($topLevelDirectory, ".config", "allowed-licenses.json")
-
-Ensure-Variable -Variable { $outputRootArtifactsDirectory } -ExitIfNullOrEmpty
-Ensure-Variable -Variable { $outputRootReportResultsDirectory } -ExitIfNullOrEmpty
-Ensure-Variable -Variable { $targetConfigAllowedLicenses } -ExitIfNullOrEmpty
-
-[System.IO.Directory]::CreateDirectory($outputRootArtifactsDirectory) | Out-Null
-[System.IO.Directory]::CreateDirectory($outputRootReportResultsDirectory) | Out-Null
-
-if (-not $isCiCd) { Delete-FilesByPattern -Path "$outputRootArtifactsDirectory" -Pattern "*"  }
-if (-not $isCiCd) { Delete-FilesByPattern -Path "$outputRootReportResultsDirectory" -Pattern "*"  }
 

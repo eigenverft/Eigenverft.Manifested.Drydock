0fc535eb31bae344a43656532e5187c026f4c839
Eigenverft <eigenverft@outlook.com>
2025-10-05 11:45:49 +0200

    update

--- .github/workflows/cicd.ps1
+++ .github/workflows/cicd.ps1
@@ -19,108 +19,19 @@ Install-Module -Name BlackBytesBox.Manifested.Version -Repository "PSGallery" -F
 Install-Module -Name BlackBytesBox.Manifested.Git -Repository "PSGallery" -Force -AllowClobber
 
 
-$result1 = Convert-DateTimeTo64SecPowershellVersion -VersionBuild 0
-#$result2 = Get-GitCurrentBranch
-#$result4 = Get-GitCurrentBranchRoot
-$result3 = Get-GitTopLevelDirectory
-
-
+$GeneratedPowershellVersion = Convert-DateTimeTo64SecPowershellVersion -VersionBuild 0
+$GitTopLevelDirectory = Get-GitTopLevelDirectory
 ##############################
 
 # Define the path to your module folder (adjust "MyModule" as needed)
-$moduleFolder = "$result3/source/Eigenverft.Manifested.Drydock"
-Update-ManifestModuleVersion -ManifestPath "$moduleFolder" -NewVersion "$($result1.VersionBuild).$($result1.VersionMajor).$($result1.VersionMinor)"
+$moduleFolder = "$GitTopLevelDirectory/source/Eigenverft.Manifested.Drydock"
+Update-ManifestModuleVersion -ManifestPath "$moduleFolder" -NewVersion "$($GeneratedPowershellVersion.VersionBuild).$($GeneratedPowershellVersion.VersionMajor).$($GeneratedPowershellVersion.VersionMinor)"
 $moduleManifest = "$moduleFolder/Eigenverft.Manifested.Drydock.psd1" -replace '[/\\]', [System.IO.Path]::DirectorySeparatorChar
 
 # Validate the module manifest
 Write-Host "===> Testing module manifest at: $moduleManifest" -ForegroundColor Cyan
 Test-ModuleManifest -Path $moduleManifest
 
-
-exit
-
 Publish-Module -Path $moduleFolder -Repository "PSGallery" -NuGetApiKey "$POWERSHELL_GALLERY"
 
-
-##############################
-# Git operations: commit changes, tag the repo with the new version, and push them
-
-$gitUserLocal = git config user.name
-$gitMailLocal = git config user.email
-
-$gitTempUser = "Workflow"
-$gitTempMail = "carstenriedel@outlook.com"  # Assuming a placeholder email
-
-git config user.name $gitTempUser
-git config user.email $gitTempMail
-
-# Define the new version tag based on the version information
-$tag = "$($result1.VersionBuild).$($result1.VersionMajor).$($result1.VersionMinor)"
-
-# Change directory to the repository's top-level directory
-Set-Location -Path $result3
-
-# Stage all changes (adjust if you want to be more specific)
-git add .
-
-# Commit changes with a message including the version tag and [skip ci] to avoid triggering GitHub Actions
-git commit -m "Update module version to $tag [skip ci]"
-
-# Create a Git tag for the new version
-git tag $tag
-
-# Push the commit and tag to the remote repository
-git push origin HEAD
-git push origin $tag
-
-git config user.name $gitUserLocal
-git config user.email $gitMailLocal
-
-exit 0
-
-# Use for cleaning local enviroment only, use channelRoot for deployment.
-$isCiCd = $false
-$isLocal = $false
-if ($env:GITHUB_ACTIONS -ieq "true")
-{
-    $isCiCd = $true
-}
-else {
-    $isLocal = $true
-}
-
-# Check if the secrets file exists before importing
-if (Test-Path "$PSScriptRoot/cicd_secrets.ps1") {
-    . "$PSScriptRoot\cicd_secrets.ps1"
-    Write-Host "Secrets loaded from file."
-} else {
-    $NUGET_GITHUB_PUSH = $args[0]
-    $NUGET_PAT = $args[1]
-    $NUGET_TEST_PAT = $args[2]
-    Write-Host "Secrets will be taken from args."
-}
-
-
-$result1 = Convert-DateTimeTo64SecPowershellVersion -VersionBuild 0
-$result2 = Get-GitCurrentBranch
-$result4 = Get-GitCurrentBranchRoot
-$result3 = Get-GitTopLevelDirectory
-
-
-##############################
-
-# Define the path to your module folder (adjust "MyModule" as needed)
-$moduleFolder = "$result3/source/BlackBytesBox.Manifested.Git"
-Update-ManifestModuleVersion -ManifestPath "$moduleFolder" -NewVersion "$($result1.VersionBuild).$($result1.VersionMajor).$($result1.VersionMinor)"
-$moduleManifest = "$moduleFolder/BlackBytesBox.Manifested.Git.psd1" -replace '[/\\]', [System.IO.Path]::DirectorySeparatorChar
-
-# Validate the module manifest
-Write-Host "===> Testing module manifest at: $moduleManifest" -ForegroundColor Cyan
-Test-ModuleManifest -Path $moduleManifest
-
 exit
-
-Publish-Module -Path $moduleFolder -Repository "PSGallery" -NuGetApiKey "$POWERSHELL_GALLERY"
-
-exit 0
-

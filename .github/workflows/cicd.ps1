31067f104e8c59fe3323bd4eda3cde345ac2c7f5
Eigenverft <eigenverft@outlook.com>
2025-10-05 12:12:01 +0200

    test add

--- .github/workflows/cicd.ps1
+++ .github/workflows/cicd.ps1
@@ -20,14 +20,14 @@ Install-Module -Name BlackBytesBox.Manifested.Git -Repository "PSGallery" -Force
 
 
 $GeneratedPowershellVersion = Convert-DateTimeTo64SecPowershellVersion -VersionBuild 0
-$GitTopLevelDirectory = Get-GitTopLevelDirectory
+$gitTopLevelDirectory = Get-GitTopLevelDirectory
 $gitCurrentBranch = Get-GitCurrentBranch
 $gitCurrentBranchRoot = Get-GitCurrentBranchRoot
 $gitRepositoryName = Get-GitRepositoryName
 ##############################
 
 # Define the path to your module folder (adjust "MyModule" as needed)
-$moduleFolder = "$GitTopLevelDirectory/source/Eigenverft.Manifested.Drydock"
+$moduleFolder = "$gitTopLevelDirectory/source/Eigenverft.Manifested.Drydock"
 Update-ManifestModuleVersion -ManifestPath "$moduleFolder" -NewVersion "$($GeneratedPowershellVersion.VersionBuild).$($GeneratedPowershellVersion.VersionMajor).$($GeneratedPowershellVersion.VersionMinor)"
 $moduleManifest = "$moduleFolder/Eigenverft.Manifested.Drydock.psd1" -replace '[/\\]', [System.IO.Path]::DirectorySeparatorChar
 
@@ -36,9 +36,15 @@ Write-Host "===> Testing module manifest at: $moduleManifest" -ForegroundColor C
 Test-ModuleManifest -Path $moduleManifest
 
 
+Write-Host "===> gitTopLevelDirectory at: $gitTopLevelDirectory" -ForegroundColor Cyan
 Write-Host "===> gitCurrentBranch at: $gitCurrentBranch" -ForegroundColor Cyan
 Write-Host "===> gitCurrentBranchRoot at: $gitCurrentBranchRoot" -ForegroundColor Cyan
 Write-Host "===> gitRepositoryName at: $gitRepositoryName" -ForegroundColor Cyan
+
+
+#Stage only module changes
+
+git -C $gitTopLevelDirectory add -- "$moduleDir"/*.ps*
 
 exit
 
